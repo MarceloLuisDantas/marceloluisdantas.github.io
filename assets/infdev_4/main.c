dbf8b463b911aa58d798464432d262f41a5c47bf
Marcelo Luis <marceloluis257gh@gmail.com>
2025-11-04 06:20:50 -0300

    infdev 4

--- assets/infdev_4/main.c
+++ assets/infdev_4/main.c
@@ -1,36 +1,51 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int soma_20(int x) {
-    return x + 20;
+typedef struct Pessoa {
+    char *nome;
+    int idade;
+    char *(*toString)(struct Pessoa*);
+    void (*free)(struct Pessoa*);
+} Pessoa;
+
+char *toStringPessoa(Pessoa *p) {
+    // +1 para o caracteres nulo \0 no final da string
+    int tamanho = snprintf(NULL, 0, "Nome: %s\nIdade: %d", p->nome, p->idade) + 1;
+    
+    char *resultado = malloc(tamanho);
+    if (!resultado) return NULL;
+    
+    snprintf(resultado, tamanho, "Nome: %s\nIdade: %d", p->nome, p->idade);
+    return resultado;
 }
 
-int quadrado(int x) {
-    return x * x; 
+void freePessoa(Pessoa *p) {
+    free(p->nome);
+    free(p);
+    p = NULL;
 }
 
-void apply(int *x, int len, int (*fun)(int)) {
-    for (int i = 0; i < len; i++)
-        x[i] = fun(x[i]);
-}
+// Metodo construtor
+Pessoa *newPessoa(char *nome, int idade) {
+    Pessoa *p = malloc(sizeof(Pessoa)); 
+    if (!p) return NULL;
 
-void print_lista(int *x, int len) {
-    printf("Lista: ");
-    for (int i = 0; i < len; i++)
-        printf("%d ", x[i]);
-    printf("\n");
+    p->nome = malloc(strlen(nome) + 1);
+    memcpy(p->nome, nome, strlen(nome) + 1);
+
+    p->idade = idade;
+    p->toString = toStringPessoa;
+    p->free = freePessoa;
+
+    return p;
 }
 
 int main() {
-    int lista[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int len = sizeof(lista)/sizeof(int); // sizeof(lista) indica o tamanho da lista em bytes, sizeof(int) diz o tamanho de um int em bytes
-     
-    print_lista(&lista[0], len);      // Lista: 1 2 3 4 5 6 7 8 9 10
-    
-    apply(&lista[0], len, &quadrado);
-    print_lista(&lista[0], len);      // Lista: 1 4 9 16 25 36 49 64 81 100 
+    Pessoa *p1 = newPessoa("Marceline", 23);
+    if (!p1) return -1;
 
-    apply(&lista[0], len, &soma_20);
-    print_lista(&lista[0], len);      // Lista: 21 24 29 36 45 56 69 84 101 120 
-
+    printf("%s\n", p1->toString(p1));
+    p1->free(p1);
     return 0;
 }

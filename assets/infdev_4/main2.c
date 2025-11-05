3f4b828994556820a9324d3ae857aa9e2e460322
Marcelo <marceloluis257gh@gmail.com>
2025-11-05 16:45:52 -0300

    tipos

---
+++ assets/infdev_4/main2.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int64_t v1 = 90;
+    long v2 = 192;
+
+    printf("Tamanho de int64_t: %ld\n", sizeof(int64_t));
+    printf("Tamanho de long: %ld\n", sizeof(long));
+}
